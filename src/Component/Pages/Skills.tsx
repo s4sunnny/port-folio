d2efd841f8ccf9f38c6c1635aa18dc0b117e01e8
Sunny <pandeysunny01@gmail.com>
2024-04-27 08:39:36 +0000

    updated skills section

--- src/Component/Pages/Skills.tsx
+++ src/Component/Pages/Skills.tsx
@@ -1,53 +1,10 @@
-import Progressbar from "./Progressbar";
-import SkillsList from "../Utils/SkillsList";
+import SkillsLogoList from "../Utils/SkillsLogoList";
 
 function Skills() {
-  const aar = SkillsList();
-  const frontEnd = aar[0].frontend.map((object, index) => {
-    return (
-      <Progressbar
-        key={index}
-        skillName={object.skillName}
-        circlePercent={object.circlePercent}
-        color={object.color}
-        textSize={object.textSize}
-      />
-    );
-  });
-
-  const backEnd = aar[0].backend.map((object, index) => {
-    return (
-      <Progressbar
-        key={index}
-        skillName={object.skillName}
-        circlePercent={object.circlePercent}
-        color={object.color}
-        textSize={object.textSize}
-      />
-    );
-  });
-  const database = aar[0].database.map((object, index) => {
-    return (
-      <Progressbar
-        key={index}
-        skillName={object.skillName}
-        circlePercent={object.circlePercent}
-        color={object.color}
-        textSize={object.textSize}
-      />
-    );
-  });
-
   return (
     <>
-      {/* <div className="h-full mx-auto py-10">
-        <div className="flex mx-auto text-white justify-between w-5/6 ">
-          <div></div>
-          {skills}
-        </div>
-      </div> */}
       <div className="h-full mx-auto py-10 px-20">
-        <div className="block rounded-lg shadow-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-black">
+        <div className="block py-10 rounded-lg shadow-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-black">
           <div className="border-b-2 font-extrabold border-neutral-100 px-6 py-3 dark:border-black/10">
             What I know
           </div>
@@ -56,7 +13,20 @@ function Skills() {
               Frontend--
             </div>
             <div className="startBar flex mx-auto text-white justify-start">
-              {frontEnd}
+              {SkillsLogoList().frontend.map((obj, index) => {
+                return (
+                  <>
+                    <div className="text-center px-4">
+                      <img
+                        className="object-fill h-20 w10"
+                        key={index}
+                        src={obj.image}
+                      />
+                      <label className="text-black">{obj.name}</label>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
           <div className="py-4">
@@ -64,7 +34,20 @@ function Skills() {
               Backend--
             </div>
             <div className="startBar flex mx-auto text-white justify-start w-5/6">
-              {backEnd}
+              {SkillsLogoList().backend.map((obj, index) => {
+                return (
+                  <>
+                    <div className="text-center px-4">
+                      <img
+                        className="object-fill h-20 w10"
+                        key={index}
+                        src={obj.image}
+                      />
+                      <label className="text-black">{obj.name}</label>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
           <div className="py-4">
@@ -72,7 +55,20 @@ function Skills() {
               Database--
             </div>
             <div className="startBar flex mx-auto text-white justify-start w-5/6">
-              {database}
+              {SkillsLogoList().database.map((obj, index) => {
+                return (
+                  <>
+                    <div className="text-center px-4">
+                      <img
+                        className="object-fill h-20 w10"
+                        key={index}
+                        src={obj.image}
+                      />
+                      <label className="text-black">{obj.name}</label>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </div>
---
+++ src/Component/Utils/SkillsLogoList.tsx
@@ -0,0 +1,84 @@
+const SkillsLogoList = () => {
+  return {
+    frontend: [
+      {
+        name: "Angular",
+        image: "images/skillsLogo/angular.svg",
+      },
+      {
+        name: "ReactJs",
+        image: "images/skillsLogo/react.svg",
+      },
+      {
+        name: "Html",
+        image: "images/skillsLogo/html.svg",
+      },
+      {
+        name: "CSS",
+        image: "images/skillsLogo/css.svg",
+      },
+      {
+        name: "Javascript",
+        image: "images/skillsLogo/js.png",
+      },
+      {
+        name: "Typescript",
+        image: "images/skillsLogo/ts.svg",
+      },
+      {
+        name: "Bootstrap",
+        image: "images/skillsLogo/boostrap.svg",
+      },
+      {
+        name: "JQuery",
+        image: "images/skillsLogo/jquery.svg",
+      },
+      {
+        name: "Tailwind",
+        image: "images/skillsLogo/tailwind.svg",
+      },
+    ],
+    backend: [
+      {
+        name: "Java",
+        image: "images/skillsLogo/java.svg",
+      },
+      {
+        name: "Spring boot",
+        image: "images/skillsLogo/spring.svg",
+      },
+      {
+        name: "Spring mvc",
+        image: "images/skillsLogo/spring.svg",
+      },
+      {
+        name: "JPA",
+        image: "images/skillsLogo/jpa.png",
+      },
+      {
+        name: "PHP",
+        image: "images/skillsLogo/php.png",
+      },
+    ],
+    database: [
+      {
+        name: "MySql",
+        image: "images/skillsLogo/mysql.svg",
+      },
+      {
+        name: "MariaDB",
+        image: "images/skillsLogo/mariadb.svg",
+      },
+      {
+        name: "OracleDB",
+        image: "images/skillsLogo/oracle.svg",
+      },
+      {
+        name: "MongoDB",
+        image: "images/skillsLogo/mongodb.svg",
+      },
+    ],
+  };
+};
+
+export default SkillsLogoList;
